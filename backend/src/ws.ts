cbaf6cbfe8d542ffdd6f38be57d8390187b5fe07
ParasRana123 <parasrana579@gmail.com>
2025-05-23 10:51:42 +0530

    Initialised a web socket connection

---
+++ backend/src/ws.ts
@@ -0,0 +1,13 @@
+import { Server, Socket } from "socket.io";
+import { Server as HttpServer } from "http";
+import path from "path";
+import { httpstatus } from "aws-sdk/clients/glacier";
+
+export function initWs(httpServer: HttpServer) {
+    const io = new Server(httpServer , {
+        cors: {
+            origin: "*",
+            methods: ["GET" , "POST"]
+        },
+    });
+}
