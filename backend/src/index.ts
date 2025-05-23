31f6c47124f027763a570ece30ce843f88220fb2
ParasRana123 <parasrana579@gmail.com>
2025-05-23 11:35:02 +0530

    Integrated ws function in main file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,19 @@ import express from "express"
 import cors from "cors"
 import { createServer } from "http"
 import dotenv from "dotenv"
+import { initWs } from "./ws"
+import { initHttp } from "./http"
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 const httpServer = createServer(app);
 
-const port = process.env.PORT;
+initWs(httpServer);
+initHttp(app);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port , () => {
     console.log(`Listening on PORT: ${port}`)
